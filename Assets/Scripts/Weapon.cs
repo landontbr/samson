9c33012e1bb6893a8c101af16acf03781e3c551a
landontbr <trivictusco@gmail.com>
2018-11-10 14:54:25 -0600

    relink

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public int wand;
+
+    void Start()
+    {
+        wand = (int)this.transform.parent.GetComponent<SteamVR_TrackedController>().controllerIndex;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("touched");
+
+        rumbleController();
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("enemy");
+
+            if (collision.relativeVelocity.magnitude > 0)
+            {
+                Debug.Log("damage");
+            }
+        }
+
+        //audioSource.Play();
+    }
+
+    void rumbleController()
+    {
+            StartCoroutine(LongVibration(0.1f, 3999));
+    }
+
+    IEnumerator LongVibration(float length, ushort strength)
+    {
+        for (float i = 0; i < length; i += Time.deltaTime)
+        {
+            SteamVR_Controller.Input(wand).TriggerHapticPulse(strength);
+            yield return null;
+        }
+    }
+
+}
